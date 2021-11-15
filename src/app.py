e0222b879a3bb97d233e1a5a1c5d3f327fe6e186
Ashlee Tan <ash@meowbie.com>
2021-11-15 15:24:56 +0800

    add the basic layout template for the website

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# Global variables
+BRAND_NAME = "FoodFinder"  # The name of our app
+
+
 @app.route('/')
 def index():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html", BRAND_NAME=BRAND_NAME)
 
 
 if __name__ == '__main__':
