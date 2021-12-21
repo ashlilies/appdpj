881315d8e6a9497d074ce80483ad0659583ddc83
Ashlee Tan <ash@meowbie.com>
2021-12-21 12:38:54 +0800

    also separate out consumer section since LLS sucks

--- src/application/Controllers/consumer.py
+++ src/application/Controllers/consumer.py
@@ -2,7 +2,7 @@
 # New ones go here, not in __init__.
 
 # Fill your @app.routes below here...
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from application import app
 
@@ -11,5 +11,10 @@ from application import app
 @app.route('/home')
 def home():
     # render a template
-    return render_template('home.html')
+    # return render_template('home.html')
+    return redirect(url_for("consumer_home"))
 
+
+@app.route("/consumer")
+def consumer_home():
+    return render_template("consumer/home.html")
