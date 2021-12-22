15aa08bb733181846d63cb7f7ba9ff96ddc3190a
yonglinnnnnn <yongglinx@gmail.com>
2021-12-22 11:56:33 +0800

    update transaction.html

--- src/application/Models/Transaction.py
+++ src/application/Models/Transaction.py
@@ -1,11 +1,11 @@
 # xu yong lin
 
 class Transaction:
-    def __init__(self, accounts_ordered_from, option, price, coupons, ratings):
+    def __init__(self, accounts_ordered_from, option, price, used_coupons, ratings):
         self.__accounts_ordered_from = accounts_ordered_from
         self.__option = option
         self.__price = float(price)
-        self.__coupons = coupons
+        self.__used_coupons = used_coupons
         self.__ratings = float(ratings)
 
     def set_accounts_ordered_from(self, accounts):
@@ -26,26 +26,15 @@ class Transaction:
     def get_price(self):
         return self.__price
 
-    def set_coupons(self, coupons):
-        self.__coupons = coupons
+    def set_used_coupons(self, used_coupons):
+        self.__used_coupons = used_coupons
 
-    def get_coupons(self):
-        return self.__coupons
+    def get_used_coupons(self):
+        return self.__used_coupons
 
     def set_ratings(self, ratings):
         self.__ratings = ratings
 
     def get_ratings(self):
         return self.__ratings
-
-
-# account, option, price, coupons, ratings
-s = Transaction("yuenloong", "delivery", 10.20, "SPAGETIT", 5)
-# s.set_accounts_ordered_from("yuenlong")
-# s.set_option("delivery")
-# s.set_price(10.20)
-# s.set_coupons("SPAGETIT")
-# s.set_ratings(5)
-
-k = Transaction("debby", "dinein", 20.20, "SPAGETIT", 1)
 
