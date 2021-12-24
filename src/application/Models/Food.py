78e8199dad7c9fdf9c15ac0e75f88fc20a6a4a35
Ashlee Tan <ash@meowbie.com>
2021-12-24 14:31:44 +0800

    complete backend for food management

--- src/application/Models/Food.py
+++ src/application/Models/Food.py
@@ -1,47 +1,19 @@
 # Clara
 
 class Food:
-    def __init__(self, image, item_name, description, price, allergy,
-                 specification):
+    def __init__(self, image, name, description, price, allergy,
+                 specification=None):
         self.__image = image
-        self.__item_name = item_name
-        self.__description = description
-        self.__price = price
-        self.__allergy = allergy
-        self.__specification = specification
+        self.name = name
+        self.description = description
+        self.price = price
+        self.allergy = allergy
+        self.specification = specification
 
+    # Get the image as a file?
     def get_image(self):
         return self.__image
 
+    # Set the image as a file?
     def set_image(self, image):
         self.__image = image
-
-    def get_item_name(self):
-        return self.__item_name
-
-    def set_item_name(self, item_name):
-        self.__item_name = item_name
-
-    def get_description(self):
-        return self.__description
-
-    def set_description(self, description):
-        self.__description = description
-
-    def get_price(self):
-        return self.__price
-
-    def set_price(self, price):
-        self.__price = price
-
-    def get_allergy(self):
-        return self.__allergy
-
-    def set_allergy(self, allergy):
-        self.__allergy = allergy
-
-    def get_specification(self):
-        return self.__specification
-
-    def set_specification(self, specification):
-        self.__specification = specification
