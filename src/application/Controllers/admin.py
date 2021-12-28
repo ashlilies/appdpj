f36e64871a16b377722c127ddd8261275f7c59ef
ruri flora newman <92986640+rurinewman@users.noreply.github.com>
2021-12-28 22:53:31 +0800

    updates for dashboard page again - ruri

--- src/application/Controllers/admin.py
+++ src/application/Controllers/admin.py
@@ -274,23 +274,5 @@ def admin_myrestaurant():  # ruri
     return render_template("admin/restaurant.html")
 
 @app.route("/admin/dashboard")
-def graph():  # ruri
-    data = [
-        ("01-01-2020", 1597),
-        ("02-01-2020", 1456),
-        ("03-01-2020", 1908),
-        ("04-01-2020", 896),
-        ("05-01-2020", 755),
-        ("06-01-2020", 453),
-        ("07-01-2020", 1100),
-        ("08-01-2020", 1235),
-        ("09-01-2020", 1478),
-    ]
-
-    labels = []
-    values = []
-    for row in data:
-        labels.append(row[0])
-        values.append(row[1])
-
-    return render_template("admin/dashboard.html", labels=labels, values=values)
+def dashboard():  # ruri
+    return render_template("admin/dashboard.html")
