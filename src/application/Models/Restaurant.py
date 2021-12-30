cdd8654c01142e724718974e45a4a2519c3706d8
ruri flora newman <92986640+rurinewman@users.noreply.github.com>
2021-12-30 21:56:11 +0800

    updates for admin details page - pending work - ruri

--- src/application/Models/Restaurant.py
+++ src/application/Models/Restaurant.py
@@ -4,27 +4,25 @@ from application.Models.Food import Food
 
 class Restaurant:
     count_id = 0
-    def __init__(self, name, contact, open_hour, close_hour, address, tags,
-                 bank, desc, deli_fee12, deli_fee24,
-                 deli_fee46, deli_fee68, table_plan):
+    def __init__(self, name):
         Restaurant.count_id +=1
-        self.customer_id = Restaurant.count_id
+        self.rest_id = Restaurant.count_id
         self.name = name
-        self.contact = contact
-        self.open_hour = open_hour
-        self.close_hour = close_hour
-        self.address = address
-        self.tags = tags
-        self.bank = bank
-        self.desc = desc
-        self.__list_of_food = []  # a list of food objects by the restaurant
-
-        # how many KM away? 1-2 2-4 4-6 6-8 and 9+
-        self.deli_fee12 = deli_fee12
-        self.deli_fee24 = deli_fee24
-        self.deli_fee46 = deli_fee46
-        self.deli_fee68 = deli_fee68
-        self.table_plan = table_plan
+        # self.contact = contact
+        # self.open_hour = open_hour
+        # self.close_hour = close_hour
+        # self.address = address
+        # self.tags = tags
+        # self.bank = bank
+        # self.desc = desc
+        # self.__list_of_food = []  # a list of food objects by the restaurant
+        # 
+        # # how many KM away? 1-2 2-4 4-6 6-8 and 9+
+        # self.deli_fee12 = deli_fee12
+        # self.deli_fee24 = deli_fee24
+        # self.deli_fee46 = deli_fee46
+        # self.deli_fee68 = deli_fee68
+        # self.table_plan = table_plan
 
         save_db()
 
@@ -41,3 +39,9 @@ class Restaurant:
                 self.__list_of_food.remove(food)
                 del food
         save_db()
+
+    def set_rest_id(self, rest_id):
+        self.rest_id = rest_id
+
+    def get_rest_id(self):
+        return self.rest_id
