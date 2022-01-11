57322bed095bf0899c417a5bb78657c741a535ac
Ashlee Tan <ash@meowbie.com>
2022-01-11 17:41:05 +0800

    removed code that broke the whole program

--- src/application/Controllers/admin.py
+++ src/application/Controllers/admin.py
@@ -4,7 +4,7 @@
 # New routes go here, not in __init__.
 import traceback
 
-from flask import render_template, request, redirect, url_for, session, flash
+from flask import render_template, request, redirect, url_for, session, flash, Flask
 from flask_login import logout_user
 
 from application.Models.Admin import *
@@ -823,63 +823,3 @@ def retrieve_restaurant():
 #         update_user_form.remarks.data = user.get_remarks()
 #
 #         return render_template('updateUser.html', form=update_user_form)
-
-
-
-@app.route("/admin/dashboard")
-def dashboard():  # ruri
-    return render_template("admin/dashboard.html")
-
-
-import urllib.request
-import os
-from werkzeug.utils import secure_filename
-
-app = Flask(__name__)
-
-UPLOAD_FOLDER = 'static/uploads/'
-
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
-        return render_template('index.html', filename=filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
-
-
-@app.route('/display/<filename>')
-def display_image(filename):
-    # print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
-
-
-if __name__ == "__main__":
-    app.run()
