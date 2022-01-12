1801fd36396e44ca8ed207bb91e30794c514a5bf
ruri flora newman <92986640+rurinewman@users.noreply.github.com>
2022-01-12 10:49:38 +0800

    updates for admin details, retrive, and update pages- ruri

--- src/application/Controllers/admin.py
+++ src/application/Controllers/admin.py
@@ -712,31 +712,6 @@ def allowed_file(filename):
         1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
-        return render_template('index.html', filename=filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
-
-
-@app.route('/display/<filename>')
-def display_image(filename):
-    # print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
-
 
 if __name__ == "__main__":
     app.run()
