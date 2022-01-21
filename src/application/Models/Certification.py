7e9b0f273fe6d8ccafcf370ca7fd86d50f62b552
Ashlee Tan <ash@meowbie.com>
2022-01-21 17:30:23 +0800

    fixed yonglin's certification database access

--- src/application/Models/Certification.py
+++ src/application/Models/Certification.py
@@ -10,11 +10,6 @@ DB_CERT = 'certification'
 
 class Certification:
     def __init__(self, id):
-        # with shelve.open('certification', 'c') as db:
-        #     try:
-        #         Certification.count_id = db['certification_id_count']
-        #     except Exception as e:
-        #         logging.error("certification_id_count: error reading from db (%s)" % e)
         self.restaurant_id = id
         self.hygiene_cert = ''
         self.halal_cert = ''
@@ -22,10 +17,6 @@ class Certification:
         self.vegan_cert = ''
         self.noPorknoLard = ''
         self.noBeef = ''
-
-        # #  database access in Models not Controller
-        # with shelve.open('certification', 'c') as handle:
-        #     handle['certification_id_count'] = Certification.count_id
 
     # query db for a cert item by passing in the id
     def query(self, id):
@@ -35,7 +26,8 @@ class Certification:
                 return certification_dict[str(id)]
             except Exception as e:
                 print(e)
-                logging.error('Certificate: tried to query id %s but not found' % id)
+                logging.error(
+                    'Certificate: tried to query id %s but not found' % id)
                 return None
 
 
@@ -52,41 +44,44 @@ class Certification:
 
 
 # create cert
-def create_res_cert(restaurant_id, hygiene_c, halal_c, vegetarian_c, vegan_c, noporknolard, nobeef):
+def create_res_cert(restaurant_id, hygiene_c, halal_c, vegetarian_c, vegan_c,
+                    noporknolard, nobeef):
     # create new class object
-    res = Certification(restaurant_id)
-    res.hygiene_cert = hygiene_c
-    res.halal_cert = halal_c
-    res.vegetarian_cert = vegetarian_c
-    res.vegan_cert = vegan_c
-    res.noPorknoLard = noporknolard
-    res.noBeef = nobeef
-    with shelve.open(DB_CERT, 'c') as res_db:
+    cert = Certification(restaurant_id)
+    cert.hygiene_cert = hygiene_c
+    cert.halal_cert = halal_c
+    cert.vegetarian_cert = vegetarian_c
+    cert.vegan_cert = vegan_c
+    cert.noPorknoLard = noporknolard
+    cert.noBeef = nobeef
+
+    with shelve.open(DB_CERT, 'c') as cert_db:
         try:
-            res_db['certification'] = res
+            certification_dict = {}
+            if "certification" in cert_db:
+                certification_dict = cert_db["certification"]
+            certification_dict[cert.restaurant_id] = cert
+            cert_db["certification"] = certification_dict
+
             logging.info('create_res_cert: successfully created object')
         except Exception as e:
             logging.error('create_res_cert: fail to access db (%s)' % e)
 
 
 # read cert
-def read_res_cert():
-    cert_array = []
-    with shelve.open(DB_CERT, 'c') as res_db:
+def read_res_cert(restaurant_id):
+    with shelve.open(DB_CERT, 'c') as cert_db:
         try:
-            cert_array = res_db['certification']
-            print('existing:', cert_array)
+            if "certification" in cert_db:
+                certification_dict = cert_db["certification"]
+                return certification_dict[restaurant_id]
+
             logging.info('read_res_cert: reading from db')
-            # if 'certification' in res_db:
-            #     cert_dict = res_db[restaurant_id]
-            #     print('existing', cert_dict)
-            # else:
-            #     res_db[restaurant_id] = cert_dict
-            #     logging.info('read_res_cert: nothing found in db, starting empty')
+
         except Exception as e:
             logging.error('read_res_cert: fail to read from db (%s)' % e)
 
-    return cert_array
+    return None
 
 # update cert
 
