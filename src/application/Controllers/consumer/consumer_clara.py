1bbcdecfefccbe510cfd641d2a699b7905f6fb21
x3BoffiNg <85842044+x3BoffiNg@users.noreply.github.com>
2022-02-01 02:50:47 +0800

    My food delivery pop up modal is pretty much done? I just have to integrate later to get the particular food that the user clicked into. Thinking if should include the add on  :/

--- src/application/Controllers/consumer/consumer_clara.py
+++ src/application/Controllers/consumer/consumer_clara.py
@@ -1,3 +1,26 @@
+from flask import render_template
+from flask_login import login_required, current_user
+
 from application import app
 
 # <------------------------- CLARA ------------------------------>
+from application.Controllers.admin.admin_ashlee import admin_side
+from application.Models.Food2 import FoodDao
+
+#
+# @app.route('/consumer/foodModal', methods=['GET', 'POST'])
+# # @login_required
+# # @admin_side
+# def foodModal():
+#
+#     return render_template('consumer/deliveryFoodMenu.html')
+
+@app.route("/consumer/foodModal")
+#displaying of food
+@login_required
+@admin_side
+def consumer_retrieve_food_modal():
+    #retrieve the food created from FoodDao
+    #using the currernt users restaurant_id
+    food_list = FoodDao.get_foods(current_user.restaurant_id)
+    return render_template('consumer/deliveryFoodMenu.html', food_list=food_list)
