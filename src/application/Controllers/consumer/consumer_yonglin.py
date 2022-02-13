21a6ed9ad6efd411e65a4cd2c4812d06311c8949
yonglinnnnnn <yongglinx@gmail.com>
2022-02-14 00:42:14 +0800

    display map

--- src/application/Controllers/consumer/consumer_yonglin.py
+++ src/application/Controllers/consumer/consumer_yonglin.py
@@ -2,13 +2,14 @@ import logging
 import shelve
 
 import overpy as overpy
-from flask import render_template, request, redirect, url_for
-from flask_login import current_user
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import current_user, login_required
 from flask_googlemaps import GoogleMaps
 
 from geopy.geocoders import Nominatim
 
 from application import app
+from application.Controllers.consumer.consumer_ashlee import consumer_side
 from application.Models.Address import ConsumerAddress
 from application.consumer_address_form import ConsumerAddressForm
 
@@ -19,14 +20,15 @@ GoogleMaps(app)
 
 
 @app.route("/myAddress", methods=["GET", "POST"])
+@login_required
+@consumer_side
 def consumer_myaddress():
     consumer_address_form = ConsumerAddressForm(request.form)
     # controller will be the place where we do all the interaction
     # address_form = ConsumerAddressForm(request.form)
-    lonlist = []
-    latlist = []
+    longitude = None
+    latitude = None
 
-    # todo: change attributes to private attributes (for 'safety' reason)
     if request.method == 'POST' and consumer_address_form.validate():
         # saving / updating the user's address
         address_dict = {}
@@ -38,20 +40,23 @@ def consumer_myaddress():
 
             # implementation of api
             try:
-                # todo: limit area to Singapore Region
                 location = geolocator.geocode(consumer_address_form.consumer_address.data)
-                latlist.append(location.latitude)
-                lonlist.append(location.longitude)
+                print(location.address)
+                longitude = location.longitude
+                latitude = location.latitude
+                flash("Successfully Saved your delivery address!")
+
+                consumer = ConsumerAddress(current_user.account_id, location.address, latitude, longitude)
+                address_dict[current_user.account_id] = consumer
+                print(consumer.get_latitude(), consumer.get_longitude())
+
             except Exception as e:
                 # todo: input showing of error as a pop out page at the top -- follow Ashlee's code
                 logging.error('Error in Address (%s)' % e)
+                flash("Could not find your address, please try again")
 
-            consumer = ConsumerAddress(current_user.account_id, location.address)
-            address_dict[current_user.account_id] = consumer
-
-            # current_user.checkout_address = consumer_address_form.consumer_address
             db['address'] = address_dict
-        return render_template("consumer/address.html", form=consumer_address_form, lonlist=lonlist, latlist=latlist)
+        return render_template("consumer/address.html", form=consumer_address_form, latitude=latitude, longitude=longitude)
     else:
         address_dict = {}
         with shelve.open('address.db', 'c') as db:
@@ -59,6 +64,7 @@ def consumer_myaddress():
                 address_dict = db['address']
                 consumer = address_dict.get(current_user.account_id)
                 consumer_address_form.consumer_address.data = consumer.address
+                print(consumer.address)
             except Exception as e:
                 logging.error('address: unable to display address due to %s in db' % e)
 
--- src/application/Models/Address.py
+++ src/application/Models/Address.py
@@ -1,9 +1,23 @@
 # XU YONG LIN
 # Address field - Consumer
 class ConsumerAddress:
-    def __init__(self, account_id, address):
+    def __init__(self, account_id, address, latitude, longitude):
         self.account_id = account_id
         self.address = address
+        self.__latitude = latitude
+        self.__longitude = longitude
+
+    def set_latitude(self, latitude):
+        self.__latitude = latitude
+
+    def set_longitude(self, longitude):
+        self.__longitude = longitude
+
+    def get_latitude(self):
+        return self.__latitude
+
+    def get_longitude(self):
+        return self.__longitude
 
     # def set_accound_id(self, account_id):
     #     self.__account_id = account_id
