41e44cc2108ebe0a8e8e88cd57d40d78b5a95ae3
yonglinnnnnn <yongglinx@gmail.com>
2022-02-14 21:15:05 +0800

    add del_time and del_fee

--- src/application/Controllers/consumer/consumer_yonglin.py
+++ src/application/Controllers/consumer/consumer_yonglin.py
@@ -15,7 +15,7 @@ from geopy.distance import geodesic
 
 from application import app
 from application.Controllers.consumer.consumer_ashlee import consumer_side
-from application.Models.Address import ConsumerAddress, calculate_distance
+from application.Models.Address import ConsumerAddress, calculate_distance, calculate_deltime_bydist, delivery_fee
 from application.Models.Cart import CartDao, Cart
 from application.Models.RestaurantSystem import RestaurantSystem
 from application.consumer_address_form import ConsumerAddressForm
@@ -36,6 +36,8 @@ def consumer_myaddress(restaurant_id):
     longitude = None
     latitude = None
     location = None
+    del_time = None
+    del_fee = None
 
     if request.method == 'POST' and consumer_address_form.validate():
         address_dict = {}
@@ -58,7 +60,9 @@ def consumer_myaddress(restaurant_id):
                 latitude = consumer.get_latitude()
                 longitude = consumer.get_longitude()
                 location = consumer.address
-                calculate_distance(latitude, longitude, restaurant)
+                distance = calculate_distance(latitude, longitude, restaurant)
+                del_time = calculate_deltime_bydist(distance)
+                del_fee = delivery_fee(distance, restaurant)
 
             except Exception as e:
                 logging.error('Error in Address (%s)' % e)
@@ -68,10 +72,6 @@ def consumer_myaddress(restaurant_id):
 
         cart = CartDao.get_cart(current_user.cart)
         cart_items = cart.get_cart_items()
-
-        return render_template("consumer/address.html", form=consumer_address_form, location=location,
-                               latitude=latitude,
-                               longitude=longitude, cart=cart, cart_items=cart_items, restaurant=restaurant)
     else:
         address_dict = {}
         with shelve.open('address.db', 'c') as db:
@@ -86,70 +86,6 @@ def consumer_myaddress(restaurant_id):
         cart = CartDao.get_cart(current_user.cart)
         cart_items = cart.get_cart_items()
 
-        return render_template("consumer/address.html", cart=cart, cart_items=cart_items,
-                               count=len(cart_items), form=consumer_address_form, location=location, latitude=latitude,
-                               longitude=longitude, restaurant=restaurant)
-
-
-# @app.route("/myAddress", methods=["GET", "POST"])
-# @login_required
-# @consumer_side
-# def consumer_myaddress():
-#     consumer_address_form = ConsumerAddressForm(request.form)
-#     longitude = None
-#     latitude = None
-#
-#     if request.method == 'POST' and consumer_address_form.validate():
-#         # saving / updating the user's address
-#         address_dict = {}
-#         with shelve.open('address.db', 'c') as db:
-#             try:
-#                 address_dict = db['address']
-#             except Exception as e:
-#                 print('Error in retrieving address from address.db (%s)' % e)
-#
-#             # implementation of api
-#             try:
-#                 location = geolocator.geocode(consumer_address_form.consumer_address.data)
-#                 print(location.address)
-#                 longitude = location.longitude
-#                 latitude = location.latitude
-#                 flash("Successfully Saved your delivery address!")
-#
-#                 consumer = ConsumerAddress(current_user.account_id, location.address, latitude, longitude)
-#                 address_dict[current_user.account_id] = consumer
-#                 print(consumer.get_latitude(), consumer.get_longitude())
-#
-#             except Exception as e:
-#                 logging.error('Error in Address (%s)' % e)
-#                 flash("Could not find your address, please try again")
-#             # writeback
-#             db['address'] = address_dict
-#         # cart = CartDao.get_cart(current_user.cart)
-#         # cart_items = cart.get_cart_items()
-#
-#         return render_template("consumer/address.html", form=consumer_address_form, latitude=latitude,
-#                                longitude=longitude)
-#     else:
-#         address_dict = {}
-#         with shelve.open('address.db', 'c') as db:
-#             try:
-#                 address_dict = db['address']
-#                 consumer = address_dict.get(current_user.account_id)
-#                 consumer_address_form.consumer_address.data = consumer.address
-#                 latitude = consumer.get_latitude()
-#                 longitude = consumer.get_longitude()
-#                 print(consumer.address)
-#             except Exception as e:
-#                 logging.error('address: unable to display address due to %s in db' % e)
-#
-#         cart = CartDao.get_cart(current_user.cart)
-#         cart_items = cart.get_cart_items()
-#
-#         for item in cart_items:
-#             print(item.food.name)
-#             print(item.qty)
-#         return render_template("consumer/address.html", cart=cart, cart_items=cart_items,
-#                                count=len(cart_items), form=consumer_address_form, latitude=latitude, longitude=longitude)
-#
-#
+    return render_template("consumer/address.html", form=consumer_address_form, location=location,
+                           latitude=latitude,
+                           longitude=longitude, cart=cart, cart_items=cart_items, restaurant=restaurant, del_time=del_time, del_fee=del_fee)
--- src/application/Models/Address.py
+++ src/application/Models/Address.py
@@ -5,6 +5,7 @@ from math import radians
 import geopy
 from geopy.distance import geodesic
 
+
 class ConsumerAddress:
     def __init__(self, account_id, address, latitude, longitude):
         self.account_id = account_id
@@ -25,9 +26,8 @@ class ConsumerAddress:
         return self.__latitude
 
 
-# def
-
 def calculate_distance(conLatitude, conLongitude, restaurant):
+    restaurant = restaurant
     resLatitude = restaurant.latitude
     resLongitude = restaurant.longitude
 
@@ -39,17 +39,38 @@ def calculate_distance(conLatitude, conLongitude, restaurant):
     restaurant_longitude = radians(resLongitude)
     restaurant_latlon = (restaurant_latitude, restaurant_longitude)
 
-    res_con_dist = (geopy.distance.distance(consumer_latlon, restaurant_latlon)*100)
+    res_con_dist = (geopy.distance.distance(consumer_latlon, restaurant_latlon) * 100)
     res_con_dist = float(str(res_con_dist)[:-3])
 
-    print(res_con_dist)
-    calculate_deltime_bydist(res_con_dist)
+    # print('distance',res_con_dist)
+    # calculate_deltime_bydist(res_con_dist)
+    # delivery_fee(res_con_dist, restaurant)
+    return res_con_dist
 
 
 def calculate_deltime_bydist(res_con_dist):
     prep_time = 15
-    est_del_time = 5 # estimate per 2km
-    duration = res_con_dist/2
-    total_time = (est_del_time*duration) + prep_time
+    est_del_time = 5  # estimate per 2km
+    duration = res_con_dist / 2
+    total_time = (est_del_time * duration) + prep_time
 
-    print(total_time)
+    # print('total time',total_time)
+    return total_time
+
+
+def delivery_fee(res_con_dist, restaurant):
+    del_fee = 0
+    # 1-2, 2-4, 4-6, 6-8, more than 9
+    if res_con_dist > 0 and res_con_dist <= 2:
+        del_fee = restaurant.del1
+    elif res_con_dist > 2 and res_con_dist <= 4:
+        del_fee = restaurant.del2
+    elif res_con_dist > 4 and res_con_dist <= 6:
+        del_fee = restaurant.del3
+    elif res_con_dist > 6 and res_con_dist <= 8:
+        del_fee = restaurant.del4
+    else:
+        del_fee = restaurant.del5
+
+    # print('del_fee',del_fee)
+    return del_fee
