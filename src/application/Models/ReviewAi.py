8a2f0ad7bc0ea34e7596775bcacebf7b5c57b7bf
Ashlee Tan <ash@meowbie.com>
2022-02-14 10:00:39 +0800

    basic reviewai

---
+++ src/application/Models/ReviewAi.py
@@ -0,0 +1,23 @@
+# Ashlee
+# AI for detecting fake reviews.
+
+# for data
+import pandas as pd
+import numpy as np
+
+# for plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# for statistical tests
+import scipy
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+
+# for machine learning
+from sklearn import model_selection, preprocessing, feature_selection
+from sklearn import ensemble, linear_model, metrics, decomposition
+
+# for explainer
+from lime import lime_tabular
+
